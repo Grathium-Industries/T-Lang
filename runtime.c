77af24ade607796f00a602b761e7e1c2d32ebc9d
Grathium Industries <GrathiumIndustries@gmail.com>
2020-05-15 16:28:20 +1000

    Optimization

--- runtime.c
+++ runtime.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "program.c"
 
 int main(int argc, const char *argv[])
 {
-
-	// MAIN
 	FILE *filePointer;
     char ch;
     filePointer = fopen(argv[1], "r");
@@ -24,31 +21,4 @@ int main(int argc, const char *argv[])
 
 	printf("\n=====End=====\n.");
 	return 0;
-}
-
-int countChar(char filename[128]) {
-	FILE* fp; 
-  
-    // Character counter (result) 
-    int count = 0;
-    char c; 
-  
-    fp = fopen(filename, "r"); 
-    if (fp == NULL) { 
-        printf("Could not open file %s", 
-               filename); 
-        return 0; 
-    } 
-  
-    // Extract characters from file 
-    // and store in character c 
-    for (c = getc(fp); c != EOF; c = getc(fp)) 
-  
-        // Increment count for this character 
-        count = count + 1; 
-  
-    // Close the file 
-    fclose(fp); 
-  
-    return count; 
 }
