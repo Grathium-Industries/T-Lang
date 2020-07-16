9462994047b08cc73af24a9dca9864f9cdc2dee6
Grathium Industries <GrathiumIndustries@gmail.com>
2020-07-16 19:59:29 +1000

    identation errors

--- runtime.c
+++ runtime.c
@@ -6,27 +6,27 @@ typedef enum { false, true } bool;
 int main(int argc, const char *argv[])
 {
 	FILE *filePointer;
-    char ch;
+	char ch;
 	bool commenting = false;
-    filePointer = fopen(argv[1], "r");
+	filePointer = fopen(argv[1], "r");
 
-    // readfile and call program
-    while ((ch = fgetc(filePointer)) != EOF) {
+	// readfile and call program
+	while ((ch = fgetc(filePointer)) != EOF) {
 		// comments
 		if (ch == ';') commenting = !commenting;
 		if (commenting == true) continue;
 
 		// binary string reader
-    	if (ch == '0') {
-    		func0();
-    	} else if (ch == '1') {
-    		func1();
-    	} else if (ch == ' ') {
+		if (ch == '0') {
+			func0();
+		} else if (ch == '1') {
+			func1();
+		} else if (ch == ' ') {
 			push();
 		}
-    }
+	}
 
-    fclose(filePointer); // close file
+	fclose(filePointer); // close file
 
 	printf("\n=====End=====\n.");
 	return 0;
